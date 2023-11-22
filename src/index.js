60ee2ce8952a1fdb3ce779f75828e2b780058316
alexandrrusskih <axl.18r@gmail.com>
2023-11-22 13:34:56 +0400

    run without prompt

--- src/index.js
+++ src/index.js
@@ -19,99 +19,102 @@ if (!fs.existsSync(CONFIG_NAME)) {
 const rawConfig = fs.readFileSync(CONFIG_NAME);
 let config = JSON.parse(rawConfig);
 
-prompt.get([{
-  name: 'nightscoutUrl',
-  description: 'please enter your nightscout url',
-  required: true,
-  default: config.nightscoutUrl
-}, {
-  name: 'nightscoutToken',
-  description: 'please enter your nightscout token',
-  required: false,
-  default: config.nightscoutToken
-}, {
-  name: 'libreUsername',
-  description: 'please enter your libreview username',
-  required: true,
-  default: config.libreUsername
-}, {
-  name: 'librePassword',
-  description: 'please enter your libreview password',
-  required: true,
-  default: config.librePassword
-}, {
-  name: 'year',
-  description: 'please enter the year you want to transfer to libreview',
-  required: true,
-  type: 'number',
-  default: new Date().getFullYear()
-}, {
-  name: 'month',
-  description: 'please enter the month you want to transfer to libreview',
-  required: true,
-  type: 'number',
-  default: new Date().getMonth() + 1
-}, {
-  name: 'day',
-  description: 'please enter the day you want to transfer to libreview',
-  required: true,
-  type: 'number',
-  default: new Date().getDate()
-}, {
-  name: 'count',
-  description: 'please enter amount of days before you want to transfer to libreview',
-  required: true,
-  type: 'number',
-  default: 1
-}, {
-  name: 'libreResetDevice',
-  description: 'if you have problems with your transfer, recreate your device id',
-  required: true,
-  type: 'boolean',
-  default: false
-}], function (err, result) {
-  if (err) {
-    return onErr(err);
+// if (Object.keys(config).length === 0) {
+//   prompt.get([{
+//     name: 'nightscoutUrl',
+//     description: 'please enter your nightscout url',
+//     required: true,
+//     default: config.nightscoutUrl
+//   }, {
+//     name: 'nightscoutToken',
+//     description: 'please enter your nightscout token',
+//     required: false,
+//     default: config.nightscoutToken
+//   }, {
+//     name: 'libreUsername',
+//     description: 'please enter your libreview username',
+//     required: true,
+//     default: config.libreUsername
+//   }, {
+//     name: 'librePassword',
+//     description: 'please enter your libreview password',
+//     required: true,
+//     default: config.librePassword
+//   }, {
+//     name: 'year',
+//     description: 'please enter the year you want to transfer to libreview',
+//     required: true,
+//     type: 'number',
+//     default: new Date().getFullYear()
+//   }, {
+//     name: 'month',
+//     description: 'please enter the month you want to transfer to libreview',
+//     required: true,
+//     type: 'number',
+//     default: new Date().getMonth() + 1
+//   }, {
+//     name: 'day',
+//     description: 'please enter the day you want to transfer to libreview',
+//     required: true,
+//     type: 'number',
+//     default: new Date().getDate()
+//   }, {
+//     name: 'count',
+//     description: 'please enter amount of days before you want to transfer to libreview',
+//     required: true,
+//     type: 'number',
+//     default: 1
+//   }, {
+//     name: 'libreResetDevice',
+//     description: 'if you have problems with your transfer, recreate your device id',
+//     required: true,
+//     type: 'boolean',
+//     default: false
+//   }], function (err, result) {
+//     if (err) {
+//       return onErr(err);
+//     }
+
+//     config = Object.assign({}, config, {
+//       nightscoutUrl: result.nightscoutUrl,
+//       count: result.count,
+//       nightscoutToken: result.nightscoutToken,
+//       libreUsername: result.libreUsername,
+//       librePassword: result.librePassword,
+//       libreDevice: (result.libreResetDevice || !!!config.libreDevice) ? uuid.v4().toUpperCase() : config.libreDevice
+//     });
+
+//   })
+// }
+// fs.writeFileSync(CONFIG_NAME, JSON.stringify(config));
+
+(async () => {
+  const offset = dayjs().utcOffset()
+  const djsDate = dayjs(`${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`);
+  // const djsDate = dayjs(`${result.year}-${result.month}-${result.day}`);
+  const fromDate = djsDate.subtract(config.count - 1, 'day').subtract(offset, 'minute').format('YYYY-MM-DDTHH:mm:ss');
+  const toDate = djsDate.add(1, 'day').subtract(offset, 'minute').format('YYYY-MM-DDTHH:mm:ss');
+
+  console.log('transfer time span', fromDate.gray, '-', toDate.gray);
+
+  const allData = await nightscout.getNightscoutAllEntries(config.nightscoutUrl, config.nightscoutToken, fromDate, toDate);
+
+  if (allData.glucoseEntriesScheduled.length > 0 || allData.foodEntries.length > 0 || allData.insulinEntries.length > 0) {
+    const auth = await libre.authLibreView(config.libreUsername, config.librePassword, config.libreDevice, config.libreResetDevice);
+    if (!!!auth) {
+      console.log('libre auth failed!'.red);
+
+      return;
+    }
+
+    await libre.transferLibreView(config.libreDevice, auth, allData.glucoseEntriesScheduled, allData.glucoseEntriesUnscheduled, allData.foodEntries, allData.insulinEntries);
+  }
+  else {
+    console.log('No entries'.blue);
   }
 
-  config = Object.assign({}, config, {
-    nightscoutUrl: result.nightscoutUrl,
-    nightscoutToken: result.nightscoutToken,
-    libreUsername: result.libreUsername,
-    librePassword: result.librePassword,
-    libreDevice: (result.libreResetDevice || !!!config.libreDevice) ? uuid.v4().toUpperCase() : config.libreDevice
-  });
-
-  fs.writeFileSync(CONFIG_NAME, JSON.stringify(config));
-
-  (async () => {
-
-    const offset = dayjs().utcOffset()
-
-    const djsDate = dayjs(`${result.year}-${result.month}-${result.day}`);
-    const fromDate = djsDate.subtract(result.count - 1, 'day').subtract(offset, 'minute').format('YYYY-MM-DDTHH:mm:ss');
-    const toDate = djsDate.add(1, 'day').subtract(offset, 'minute').format('YYYY-MM-DDTHH:mm:ss');
-
-    console.log('transfer time span', fromDate.gray, '-', toDate.gray);
-
-    const allData = await nightscout.getNightscoutAllEntries(config.nightscoutUrl, config.nightscoutToken, fromDate, toDate);
-
-    if (allData.glucoseEntriesScheduled.length > 0 || allData.foodEntries.length > 0 || allData.insulinEntries.length > 0) {
-      const auth = await libre.authLibreView(config.libreUsername, config.librePassword, config.libreDevice, result.libreResetDevice);
-      if (!!!auth) {
-        console.log('libre auth failed!'.red);
-
-        return;
-      }
-
-      await libre.transferLibreView(config.libreDevice, auth, allData.glucoseEntriesScheduled, allData.glucoseEntriesUnscheduled, allData.foodEntries, allData.insulinEntries);
-    }
-    else {
-      console.log('No entries'.blue);
-    }
-
-  })();
-});
+})();
+// });
 
 function onErr(err) {
   console.log(err);
