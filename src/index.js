6b939cb6b7288dc20ab87ecc0707e88ee942c43f
alexandrrusskih <axl.18r@gmail.com>
2023-12-04 10:43:01 +0400

    disable/enable new sensor install

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-const dayjs = require('dayjs');
-const uuid = require('uuid');
-const duration = require('dayjs/plugin/duration');
-const colors = require('colors');
-const prompt = require('prompt');
-const fs = require('fs');
-dayjs.extend(duration);
-require('dotenv').config({ path: __dirname + '/../config.env' });
+const dayjs = require('dayjs')
+const uuid = require('uuid')
+const duration = require('dayjs/plugin/duration')
+const colors = require('colors')
+const prompt = require('prompt')
+const fs = require('fs')
+dayjs.extend(duration)
+require('dotenv').config({ path: __dirname + '/../config.env' })
 
-const libre = require('./functions/libre');
-const nightscout = require('./functions/nightscout');
-const { log } = require('console');
-let newSensor = false;
+const libre = require('./functions/libre')
+const nightscout = require('./functions/nightscout')
+const { log } = require('console')
+let newSensor = false
 
-const CONFIG_NAME = 'config.json';
-const DEFAULT_CONFIG = {
-};
+const CONFIG_NAME = 'config.json'
+const DEFAULT_CONFIG = {}
 
 if (!fs.existsSync(CONFIG_NAME)) {
-  fs.writeFileSync(CONFIG_NAME, JSON.stringify(DEFAULT_CONFIG));
+  fs.writeFileSync(CONFIG_NAME, JSON.stringify(DEFAULT_CONFIG))
 }
 
-const rawConfig = fs.readFileSync(CONFIG_NAME);
-let config = JSON.parse(rawConfig);
+const rawConfig = fs.readFileSync(CONFIG_NAME)
+let config = JSON.parse(rawConfig)
 
 // if (Object.keys(config).length === 0) {
 //   prompt.get([{
@@ -78,68 +77,103 @@ let config = JSON.parse(rawConfig);
 //     if (err) {
 //       return onErr(err);
 //     }
-const sensorDate = dayjs(config.newSensorDate)  // дата след. установки датчика
+const sensorDate = dayjs(config.newSensorDate) // дата след. установки датчика
 const setupDate = dayjs(config.setupDate) // дата последней установки датчика
 const toDate = dayjs(new Date())
 
 const difsetup = dayjs.duration(toDate.diff(setupDate)).asDays()
 const difs = dayjs.duration(sensorDate.diff(toDate)).asDays()
 
-if (difs < 1 && difsetup > 1) {
+if (difs < 1 && difsetup > 1 && config.newSensorEnabled == '1') {
   const h = nightscout.randomInt(10, 20) // время устоановки следующего датчика
 
-  if ((toDate.format("HH")) >= (sensorDate.hour() - 1) && (toDate.format("HH")) <= (sensorDate.hour() + 1)) {
+  if (
+    toDate.format('HH') >= sensorDate.hour() - 1 &&
+    toDate.format('HH') <= sensorDate.hour() + 1
+  ) {
     newSensor = true
-    const newSensorDate = toDate.add(config.sensorDays, "days").hour(h).format("YYYY-MM-DDTHH:mm:ss")
+    const newSensorDate = toDate
+      .add(config.sensorDays, 'days')
+      .hour(h)
+      .format('YYYY-MM-DDTHH:mm:ss')
 
     config = Object.assign({}, config, {
-      newSensorDate: newSensorDate,  // новая дата установки датчика
-      setupDate: toDate.format("YYYY-MM-DD")  // дата последней установки датчика
-    });
+      newSensorDate: newSensorDate, // новая дата установки датчика
+      setupDate: toDate.format('YYYY-MM-DD') // дата последней установки датчика
+    })
     //  fs.writeFileSync(CONFIG_NAME, JSON.stringify(config));
   }
 }
 
-(async () => {
+;(async () => {
   const offset = dayjs().utcOffset()
   let needPoints = true
-  const toDate = dayjs(new Date()).subtract(offset, 'minute').format('YYYY-MM-DDTHH:mm:ss'); // текущее время
-  let fromDate = dayjs(toDate).subtract(config.hours * 60, 'minute').format('YYYY-MM-DDTHH:mm:ss');
-  if (newSensor) {  // если новый сенсор, то берём за 15 минут до текущего времени
-    fromDate = dayjs(toDate).subtract(15, 'minute').format('YYYY-MM-DDTHH:mm:ss');
+  const toDate = dayjs(new Date())
+    .subtract(offset, 'minute')
+    .format('YYYY-MM-DDTHH:mm:ss') // текущее время
+  let fromDate = dayjs(toDate)
+    .subtract(config.hours * 60, 'minute')
+    .format('YYYY-MM-DDTHH:mm:ss')
+  if (newSensor) {
+    // если новый сенсор, то берём за 15 минут до текущего времени
+    fromDate = dayjs(toDate)
+      .subtract(15, 'minute')
+      .format('YYYY-MM-DDTHH:mm:ss')
     needPoints = false
   }
-  const h = dayjs(new Date()).hour();
+  const h = dayjs(new Date()).hour()
   if (h <= 5) {
-    needPoints = false  // ставить точки не нужно
-    fromDate = dayjs(toDate).subtract(24, 'hour').format('YYYY-MM-DDTHH:mm:ss'); // берём сутки для заполнения возможных пробелов
+    needPoints = false // ставить точки не нужно
+    fromDate = dayjs(toDate).subtract(24, 'hour').format('YYYY-MM-DDTHH:mm:ss') // берём сутки для заполнения возможных пробелов
   }
 
-  console.log('transfer time span', fromDate.gray, '-', toDate.gray);
+  console.log('transfer time span', fromDate.gray, '-', toDate.gray)
 
-  const allData = await nightscout.getNightscoutAllEntries(config.nightscoutUrl, config.nightscoutToken, fromDate, toDate, parseInt(config.min_count), parseInt(config.max_count), needPoints);
+  const allData = await nightscout.getNightscoutAllEntries(
+    config.nightscoutUrl,
+    config.nightscoutToken,
+    fromDate,
+    toDate,
+    parseInt(config.min_count),
+    parseInt(config.max_count),
+    needPoints
+  )
 
-  if (allData.glucoseEntriesScheduled.length > 0 || allData.foodEntries.length > 0 || allData.insulinEntries.length > 0) {
-    const auth = await libre.authLibreView(config.libreUsername, config.librePassword, config.libreDevice, config.libreResetDevice);
+  if (
+    allData.glucoseEntriesScheduled.length > 0 ||
+    allData.foodEntries.length > 0 ||
+    allData.insulinEntries.length > 0
+  ) {
+    const auth = await libre.authLibreView(
+      config.libreUsername,
+      config.librePassword,
+      config.libreDevice,
+      config.libreResetDevice
+    )
     if (!!!auth) {
-      console.log('libre auth failed!'.red);
-      return;
+      console.log('libre auth failed!'.red)
+      return
     }
 
-    await libre.transferLibreView(config.libreDevice, auth, allData.glucoseEntriesScheduled, allData.glucoseEntriesUnscheduled, allData.foodEntries, allData.insulinEntries);
+    await libre.transferLibreView(
+      config.libreDevice,
+      auth,
+      allData.glucoseEntriesScheduled,
+      allData.glucoseEntriesUnscheduled,
+      allData.foodEntries,
+      allData.insulinEntries
+    )
     if (newSensor) {
-      await libre.transferNewSensorLibreView(config.libreDevice, auth, fromDate); //корректируем время UTC
+      await libre.transferNewSensorLibreView(config.libreDevice, auth, fromDate) //корректируем время UTC
     }
-    fs.writeFileSync(CONFIG_NAME, JSON.stringify(config));
+    fs.writeFileSync(CONFIG_NAME, JSON.stringify(config))
+  } else {
+    console.log('No entries'.blue)
   }
-  else {
-    console.log('No entries'.blue);
-  }
-
-})();
+})()
 // });
 
-function onErr(err) {
-  console.log(err);
-  return 1;
+function onErr (err) {
+  console.log(err)
+  return 1
 }
